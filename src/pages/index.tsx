5fa7029f1735fe9ac88a349dcd2bb542108ac1ab
Harry Tran <harry.tran@avancevl.com>
2023-10-05 18:40:07 +0700

    Add bank info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,10 +42,10 @@ export default function Home() {
               <div className="flex items-center gap-4 text-md">
                 <Link onClick={(e) => handleClick(e)} href="#" className="px-2 py-8">Home</Link>
                 <Link onClick={(e) => handleClick(e, 'album')} href="#album" className="px-2 py-8">Album ảnh</Link>
-                <Link onClick={(e) => handleClick(e, 'bride')} href="#detail" className="px-2 py-8">Nhà gái</Link>
-                <Link onClick={(e) => handleClick(e, 'groom')} href="#detail" className="px-2 py-8">Nhà trai</Link>
-                <Link onClick={(e) => handleClick(e, 'album')} href="#" className="px-2 py-8">Mừng cưới</Link>
-                <Link onClick={(e) => handleClick(e, 'album')} href="#" className="px-2 py-8">Gửi lời chúc</Link>
+                <Link onClick={(e) => handleClick(e, 'bride')} href="#bride" className="px-2 py-8">Nhà gái</Link>
+                <Link onClick={(e) => handleClick(e, 'groom')} href="#groom" className="px-2 py-8">Nhà trai</Link>
+                <Link onClick={(e) => handleClick(e, 'bank-info')} href="#bank-info" className="px-2 py-8">Mừng cưới</Link>
+                <Link onClick={(e) => handleClick(e, '')} href="#" className="px-2 py-8">Gửi lời chúc</Link>
               </div>
             </nav>
           </div>
@@ -62,10 +62,10 @@ export default function Home() {
               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                 <li><Link onClick={(e) => handleClick(e)} href="#">Home</Link></li>
                 <li><Link onClick={(e) => handleClick(e, 'album')} href="#album">Album ảnh</Link></li>
-                <li><Link onClick={(e) => handleClick(e, 'bride')} href="#detail">Nhà gái</Link></li>
-                <li><Link onClick={(e) => handleClick(e, 'groom')} href="#detail">Nhà trai</Link></li>
-                <li><Link onClick={(e) => handleClick(e, 'album')} href="#">Mừng cưới</Link></li>
-                <li><Link onClick={(e) => handleClick(e, 'album')} href="#">Gửi lời chúc</Link></li>
+                <li><Link onClick={(e) => handleClick(e, 'bride')} href="#bride">Nhà gái</Link></li>
+                <li><Link onClick={(e) => handleClick(e, 'groom')} href="#groom">Nhà trai</Link></li>
+                <li><Link onClick={(e) => handleClick(e, 'bank-info')} href="#bank-info">Mừng cưới</Link></li>
+                <li><Link onClick={(e) => handleClick(e, '')} href="#">Gửi lời chúc</Link></li>
               </ul>
             </div>
           </div>
@@ -124,9 +124,9 @@ export default function Home() {
             </div>
           </div>
           <div className="mt-10">
-            <div className="container hero relative m-auto bg-[url('/images/bg-3.jpg')] bg-cover md:rounded-lg">
+            <div className="container hero relative m-auto bg-[url('/images/bg-3.jpg')] bg-cover">
               <div className="hero-overlay bg-black/10"></div>
-              <div className="flex flex-col-reverse md:flex-row gap-5 md:gap-20 items-stretch md:items-center p-5 py-10 md:p-20 md:py-24">
+              <div className="flex flex-col-reverse lg:flex-row gap-5 md:gap-20 items-stretch md:items-center p-5 py-10 md:p-20 md:py-24">
                 <div className="p-10 rounded-lg backdrop-blur-lg bg-white/40 mt-5 md:mt-0">
                   <StaticCalendar date="2023/10/22" className="!max-w-full md:max-w-[350px]" />
                 </div>
@@ -170,9 +170,9 @@ export default function Home() {
             </div>
           </div>
           <div className="mt-10">
-            <div className="container hero relative m-auto bg-[url('/images/bg-4.jpg')] bg-cover md:rounded-lg text-white">
+            <div className="container hero relative m-auto bg-[url('/images/bg-4.jpg')] bg-cover text-white">
               <div className="hero-overlay bg-black/20"></div>
-              <div className="flex flex-col md:flex-row gap-5 md:gap-20 items-stretch md:items-center px-5 py-10 md:px-20 md:py-24">
+              <div className="flex flex-col lg:flex-row gap-5 md:gap-20 items-stretch md:items-center px-5 py-10 md:px-20 md:py-24">
                 <div>
                   <h3 className="text-2xl md:text-[32px] uppercase tracking-wider font-semibold">Tiệc cưới tại gia đình <br className="md:hidden" /> nhà trai</h3>
                   <div className="mt-7 text-xl leading-9">
@@ -204,6 +204,77 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <section className="mt-16 scroll-mt-20" id="bank-info">
+          <div className="container m-auto">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+              <div className="flex flex-col gap-2 justify-center py-14 mx-5 sm:mx-0">
+                <h2 className="text-4xl font-bold uppercase">Mừng cưới</h2>
+                <p className="text-xl">Nhớ ghi tên nhé!</p>
+              </div>
+              <div className="px-16 md:px-28 py-20 md:py-40 bg-[#7C36BB] text-white flex flex-col justify-center">
+                <div>
+                  <Image className="h-[35px] w-auto grayscale opacity-50" src="/images/tp-bank-logo.png" alt="" width={200} height={200} />
+                </div>
+                <div className="py-5">
+                  <div className="p-4 bg-white rounded">
+                    <Image className="h-full w-full rounded" src="/images/trang-tp-bank.png" alt="" width={250} height={250} />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-1 mt-2">
+                  <h3 className="text-lg md:text-xl font-bold uppercase">Nguyễn Thị Thu Trang</h3>
+                  <p className="text-xl">0192 8072 001</p>
+                  <p className="opacity-80">Cô dâu</p>
+                </div>
+              </div>
+              <div className="px-16 md:px-28 py-20 md:py-40 bg-[#BD2626] text-white flex flex-col justify-center">
+                <div>
+                  <Image className="h-[35px] w-auto grayscale opacity-50" src="/images/techcombank-logo.svg" alt="" width={200} height={200} />
+                </div>
+                <div className="py-5">
+                  <div className="p-4 bg-white rounded">
+                    <Image className="h-full w-full rounded" src="/images/khanh-techcombank.png" alt="" width={250} height={250} />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-1 mt-2">
+                  <h3 className="text-lg md:text-xl font-bold uppercase">Trần Xuân Khánh</h3>
+                  <p className="text-xl">1903 6301 4920 11</p>
+                  <p className="opacity-80">Chú rể</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="mt-10 py-10 md:py-20">
+          <div className="container m-auto flex flex-col justify-center items-center">
+            <h3 className="text-3xl font-semibold text-center mb-5">@ Tag us on Facebook</h3>
+            <p>Cô dâu: @Thu Trang</p>
+            <p>Chú rể: @Trần Xuân Khánh</p>
+            <p>Hashtag: #khanhtrang_wedding</p>
+          </div>
+        </section>
+
+        <section className="hero scroll-mt-20 mt-20 bg-cover bg-center md:min-h-[500px] w-full relative">
+          <div className="hero-overlay bg-white/10 z-20"></div>
+          <Image
+            layout="fill"
+            className="object-cover object-center pointer-events-none z-10"
+            src="/images/bg-7.jpg"
+            alt=""
+          />
+          <div className="container m-auto flex flex-col relative z-50 items-center px-5 py-20">
+            <h3 className="text-5xl md:text-[100px] text-white font-black text-center">Thank you!</h3>
+            <p className="text-3xl md:text-[50px] text-white font-bold text-center">See you at the wedding!</p>
+          </div>
+        </section>
+
+        <footer className="bg-[#F2F2F2]">
+          <div className="container flex items-center justify-between px-5 py-5 text-sm m-auto">
+            <h2 className="font-semibold">Khánh & Trang</h2>
+            <p>♥ 2023</p>
+          </div>
+        </footer>
       </div>
     </>
   )
